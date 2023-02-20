6b23fc5d66dccb969a3da7e60ca06f6d0b2af19d
Daniel Hillerström <daniel.hillerstrom@huawei.com>
2023-02-20 15:22:48 +0100

    Adapt Gor Nishanov's CppCon'18 demo

---
+++ GorNishanov/naive.h
@@ -0,0 +1,22 @@
+#ifndef NAIVE_H
+#define NAIVE_H
+
+template <typename Iterator>
+bool naive_binary_search(Iterator first, Iterator last, int val) {
+  auto len = last - first;
+  while (len > 0) {
+    const auto half = len / 2;
+    const auto middle = first + half;
+    const auto middle_key = *middle;
+    if (middle_key < val) {
+      first = middle + 1;
+      len = len - half - 1;
+    } else {
+      len = half;
+    }
+    if (middle_key == val)
+      return true;
+  }
+  return false;
+}
+#endif /* NAIVE_H */
